47b84911f23144901c390ed684150cc35c9d1fb5
Agostino De Marco <agostino.demarco@unina.it>
2017-03-02 13:29:06 +0100

    Test1 in sandbox2.bs ok. Loading an fxml file, using an fx:id in the main controller class.

--- JPADSandBox_v2/src/sandbox2/bs/javafx/test1/Main.java
+++ JPADSandBox_v2/src/sandbox2/bs/javafx/test1/Main.java
@@ -1,35 +1,49 @@
 package sandbox2.bs.javafx.test1;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("pippoGUI.fxml"));
-		BorderPane root = loader.load();
-		Scene scene = new Scene(root, 500, 500);
-		primaryStage.setTitle("Pippo 1");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
+	Scene theScene;
+	Parent theRoot;
 	
-	void buttonClicked() {
+	@FXML
+    private TextArea myTextArea;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+        theRoot = FXMLLoader.load(getClass().getResource("pippoGUI.fxml"));
+        
+        stage.setTitle("FXML Welcome");
+        theScene = new Scene(theRoot, 700, 500);
+        stage.setScene(theScene);
+        stage.show();
+    }
+	
+	@FXML
+	void buttonClicked() throws IOException {
 		System.out.println("Hello!");
+		// Get the current date and time
+		LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDateTime = currentTime.format(formatter);
+		myTextArea.appendText("("+formattedDateTime+") AAAA!\n");
+		
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
-
+		Application.launch(Main.class, args);
 	}
 
 
