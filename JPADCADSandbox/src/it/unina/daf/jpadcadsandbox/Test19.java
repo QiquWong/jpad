1593dfc19a565f8989344bbbec9ea5c8eace350c
Agostino De Marco <agostino.demarco@unina.it>
2017-12-29 17:03:07 +0100

    [JPADCAD+Sandbox] Test19, refinements and well established filler surface

--- JPADCADSandbox/src/it/unina/daf/jpadcadsandbox/Test19.java
+++ JPADCADSandbox/src/it/unina/daf/jpadcadsandbox/Test19.java
@@ -1,9 +1,8 @@
 package it.unina.daf.jpadcadsandbox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import javax.measure.unit.SI;
 
 import it.unina.daf.jpadcad.occ.CADGeomCurve3D;
 import it.unina.daf.jpadcad.occ.OCCEdge;
@@ -11,22 +10,11 @@ import it.unina.daf.jpadcad.occ.OCCGeomCurve3D;
 import it.unina.daf.jpadcad.occ.OCCShape;
 import it.unina.daf.jpadcad.occ.OCCUtils;
 import it.unina.daf.jpadcad.occ.OCCVertex;
-import opencascade.Adaptor3d_HCurve;
-import opencascade.BRepAdaptor_Curve;
-import opencascade.BRepAdaptor_HCurve;
 import opencascade.BRepOffsetAPI_MakeFilling;
-import opencascade.BRep_Tool;
 import opencascade.GeomAbs_Shape;
-import opencascade.GeomPlate_BuildPlateSurface;
-import opencascade.GeomPlate_CurveConstraint;
-import opencascade.Geom_Surface;
-import opencascade.TopAbs_ShapeEnum;
-import opencascade.TopExp_Explorer;
 import opencascade.TopoDS;
-import opencascade.TopoDS_Edge;
 import opencascade.TopoDS_Face;
 import opencascade.gp_Pnt;
-import processing.core.PVector;
 
 public class Test19 {
 
@@ -44,6 +32,7 @@ public class Test19 {
 		ptsSec1.add(new double[]{ 0.50, 1.00, 1.30});
 		ptsSec1.add(new double[]{ 0.50, 1.50, 0.00});
 		CADGeomCurve3D cadSec1 = OCCUtils.theFactory.newCurve3D(ptsSec1, false);
+		System.out.println(">> Section-1 parallel to plane YZ, at x = " + ptsSec1.get(0)[0]);
 		
 		// section 2
 		List<double[]> ptsSec2 = new ArrayList<double[]>();
@@ -51,6 +40,7 @@ public class Test19 {
 		ptsSec2.add(new double[]{ 2.00, 2.00, 1.45});
 		ptsSec2.add(new double[]{ 2.00, 2.50, 0.00});
 		CADGeomCurve3D cadSec2 = OCCUtils.theFactory.newCurve3D(ptsSec2, false);
+		System.out.println(">> Section-2 parallel to plane YZ, at x = " + ptsSec2.get(0)[0]);
 
 		// section 3
 		List<double[]> ptsSec3 = new ArrayList<double[]>();
@@ -58,6 +48,8 @@ public class Test19 {
 		ptsSec3.add(new double[]{ 4.00, 3.50, 3.00});
 		ptsSec3.add(new double[]{ 4.00, 4.00, 0.00});
 		CADGeomCurve3D cadSec3 = OCCUtils.theFactory.newCurve3D(ptsSec3, false);
+		System.out.println(">> Section-3 parallel to plane YZ, at x = " + ptsSec3.get(0)[0]);
+		
 
 		System.out.println("========== [main] Constructing the patch, Patch-1");
 		List<CADGeomCurve3D> cadSections = new ArrayList<>();
@@ -67,10 +59,11 @@ public class Test19 {
 		OCCShape patch1 = OCCUtils.makePatchThruSections( // -> OCCShell
 				new OCCVertex(0.0, 0.0, 0.0),
 				cadSections
-				);		
+				);
+		System.out.println(">> loft through point (0,0,0) and sections 1-2-3. Done.");
 		
 		// guide-curve-1
-		System.out.println("========== [main] Constructing the guide curve to deform Patch-1");
+		System.out.println("========== [main] Constructing the guide curves to build a filler patch");
 		// guide curve in plane XZ
 		List<double[]> ptsGC1 = new ArrayList<double[]>();
 		ptsGC1.add(new double[]{ 0.00, 0.00, 0.00});
@@ -79,15 +72,63 @@ public class Test19 {
 		double[] vX0 = new double[]{ 
 				0.1*ptsSec2.get(0)[0] + 0.9*ptsSec3.get(0)[0], // pick an X-station between sec-2 & sec-3
 				0.00, 
-				0.98*ptsSec3.get(0)[2]};
+				1.01*ptsSec3.get(0)[2]};
 		ptsGC1.add(vX0); // tweak the Z
 		ptsGC1.add(ptsSec3.get(0));		
 		CADGeomCurve3D cadGC1 = OCCUtils.theFactory.newCurve3D(ptsGC1, false);
-		
-		// plating: constrain patch-1 to guide-curve-1
+		System.out.println(">> Guide-Curve-1, in plane XZ, connecting (0,0,0) and initial points on sections 1-2-3.");
 
-		System.out.println("========== [main] Preparing the plate surface to deform Patch-1");
+		// guide curve in plane XY
+		List<double[]> ptsGC2 = new ArrayList<double[]>();
+		ptsGC2.add(new double[]{ 0.00, 0.00, 0.00});
+		ptsGC2.add(ptsSec1.get(2));
+		ptsGC2.add(ptsSec2.get(2));
+		double[] vX1 = new double[]{ 
+				0.1*ptsSec2.get(2)[0] + 0.9*ptsSec3.get(2)[0], // pick an X-station between sec-2 & sec-3
+				1.00*ptsSec3.get(2)[1], 
+				0.00};
+		ptsGC2.add(vX1); // tweak the Z
+		ptsGC2.add(ptsSec3.get(2));
+		CADGeomCurve3D cadGC2 = OCCUtils.theFactory.newCurve3D(ptsGC2, false);
+		System.out.println(">> Guide-Curve-2, in plane XY, connecting (0,0,0) and final points on sections 1-2-3.");
+		
+		// 3 POINTS ALONG SECTIONS 1-2-3
+		double[] rangeS1 = cadSec1.getRange();
+		System.out.println(">> Sec-1 range: " + Arrays.toString(rangeS1));
+		double[] vPntS1a = ((OCCGeomCurve3D)cadSec1).value(0.25*(rangeS1[1] - rangeS1[0]));
+		double[] vPntS1b = ((OCCGeomCurve3D)cadSec1).value(0.50*(rangeS1[1] - rangeS1[0]));
+		double[] vPntS1c = ((OCCGeomCurve3D)cadSec1).value(0.75*(rangeS1[1] - rangeS1[0]));
+
+		double[] rangeS2 = cadSec2.getRange();
+		System.out.println(">> Sec-2 range: " + Arrays.toString(rangeS2));
+		double[] vPntS2a = ((OCCGeomCurve3D)cadSec2).value(0.25*(rangeS2[1] - rangeS2[0]));
+		double[] vPntS2b = ((OCCGeomCurve3D)cadSec2).value(0.50*(rangeS2[1] - rangeS2[0]));
+		double[] vPntS2c = ((OCCGeomCurve3D)cadSec2).value(0.75*(rangeS2[1] - rangeS2[0]));
+
+		double[] rangeS3 = cadSec3.getRange();
+		System.out.println(">> Sec-3 range: " + Arrays.toString(rangeS3));		
+		double[] vPntS3a = ((OCCGeomCurve3D)cadSec3).value(0.25*(rangeS3[1] - rangeS3[0]));
+		double[] vPntS3b = ((OCCGeomCurve3D)cadSec3).value(0.50*(rangeS3[1] - rangeS3[0]));
+		double[] vPntS3c = ((OCCGeomCurve3D)cadSec3).value(0.75*(rangeS3[1] - rangeS3[0]));
+
+		List<double[]> ptsGCm = new ArrayList<double[]>();
+		ptsGCm.add(new double[]{ 0.00, 0.00, 0.00});
+		ptsGCm.add(vPntS1b);
+		ptsGCm.add(vPntS2b);
+//		double[] vXm = new double[]{ 
+//				0.1*vPnt3b[0] + 0.9*vPnt2b[0],
+//				1.00*ptsSec3.get(2)[1], 
+//				0.00};
+		ptsGCm.add(vPntS3b);
+
+		CADGeomCurve3D cadGCm = OCCUtils.theFactory.newCurve3D(ptsGCm, false);
+		System.out.println(">> Guide-Curve-M, connecting (0,0,0) and middle points on sections 1-2-3.");
+		
+		System.out.println("========== [main] Preparing the filler surface to deform Patch-1");
+		
 /*
+ * this approach with a plating surface does not work
+ * 
 		// plate maker
 		GeomPlate_BuildPlateSurface plateMaker = new GeomPlate_BuildPlateSurface(
 				BRep_Tool.Surface(
@@ -111,28 +152,22 @@ public class Test19 {
 //		plateMaker.Perform(); // FIXME: crashes
 
 */
-		
+		// Filler approach, that works
 		BRepOffsetAPI_MakeFilling fillMaker = new BRepOffsetAPI_MakeFilling();
+		
 		TopoDS_Face loftFace = TopoDS.ToFace( patch1.getShape() );
 		
-		// finds Edges in Face
-		TopExp_Explorer _edgeExplorer = new TopExp_Explorer(loftFace, TopAbs_ShapeEnum.TopAbs_EDGE);
-		for ( ; _edgeExplorer.More()!=0; _edgeExplorer.Next()) {
-			TopoDS_Edge _anEdge = TopoDS.ToEdge(_edgeExplorer.Current());
-			if (BRep_Tool.Degenerated(_anEdge) == 0) {
-				System.out.println("REGULAR Edge!");
-				fillMaker.Add(_anEdge, GeomAbs_Shape.GeomAbs_C0);
-			}
-			else {
-				System.out.println("DEGENERATE Edge!");				
-			}
-		}
-		
-		fillMaker.Add(new gp_Pnt(vX0[0], vX0[1]+1.0, vX0[2]+2.0));
-//		fillMaker.Add(
-//				( (OCCEdge)((OCCGeomCurve3D)cadGC1).edge() ).getShape(),
-//				GeomAbs_Shape.GeomAbs_C0
-//				);
+		fillMaker.Add(
+				( (OCCEdge)((OCCGeomCurve3D)cadGC1).edge() ).getShape(),
+				GeomAbs_Shape.GeomAbs_C0
+				);
+		fillMaker.Add(
+				( (OCCEdge)((OCCGeomCurve3D)cadGC2).edge() ).getShape(),
+				GeomAbs_Shape.GeomAbs_C0
+				);
+// 
+// Constraining filler to pass through sections 2 & 3 doesn't work
+//
 //		fillMaker.Add(
 //				( (OCCEdge)((OCCGeomCurve3D)cadSec1).edge() ).getShape(),
 //				GeomAbs_Shape.GeomAbs_C0
@@ -141,11 +176,27 @@ public class Test19 {
 //				( (OCCEdge)((OCCGeomCurve3D)cadSec2).edge() ).getShape(),
 //				GeomAbs_Shape.GeomAbs_C0
 //				);
-//		fillMaker.Add(
-//				( (OCCEdge)((OCCGeomCurve3D)cadSec3).edge() ).getShape(),
-//				GeomAbs_Shape.GeomAbs_C0
-//				);
-		fillMaker.LoadInitSurface(loftFace);
+
+		fillMaker.Add(
+				( (OCCEdge)((OCCGeomCurve3D)cadSec3).edge() ).getShape(),
+				GeomAbs_Shape.GeomAbs_C0
+				);
+
+		System.out.println(">> constraining filler surface to guide curves 1-2, and section 3.");
+		
+		fillMaker.Add(new gp_Pnt(vPntS1a[0], vPntS1a[1], vPntS1a[2]));
+
+		System.out.println(">> constraining filler surface to 1 point along section 1.");
+
+		fillMaker.Add(new gp_Pnt(vPntS2a[0], vPntS2a[1], vPntS2a[2]));
+		fillMaker.Add(new gp_Pnt(vPntS2b[0], vPntS2b[1], vPntS2b[2]));
+		fillMaker.Add(new gp_Pnt(vPntS2c[0], vPntS2c[1], vPntS2c[2]));
+
+		System.out.println(">> constraining filler surface to 3 points along section 2.");
+		
+//		fillMaker.LoadInitSurface(loftFace);
+		// DO NOT init, let OCC initialize the fillMaker surface automatically
+		
 		fillMaker.Build();
 		System.out.println("Deformed surface is done? = " + fillMaker.IsDone());
 		System.out.println("Deformed surface shape  type: " + fillMaker.Shape().ShapeType());
@@ -155,7 +206,12 @@ public class Test19 {
 		cadSections.stream()
 			.forEach(s -> shapes.add((OCCEdge)((OCCGeomCurve3D)s).edge()));
 		shapes.add((OCCEdge)((OCCGeomCurve3D)cadGC1).edge());
+		shapes.add((OCCEdge)((OCCGeomCurve3D)cadGC2).edge());
+		shapes.add((OCCEdge)((OCCGeomCurve3D)cadGCm).edge());
+		
+		// uncomment to export patch passing through sections 1-2-3 
 		//shapes.add(patch1);
+		
 		shapes.add((OCCShape)OCCUtils.theFactory.newShape(fillMaker.Shape()));
 		
 		// Write to a file
