0e4b0df3ca2eb8baf43c34be5698df4afc6d30b3
Manuela <manuela.ruocco@aol.com>
2018-05-18 14:43:00 +0200

    Bruno Spoti sandbox has been added.

---
+++ Wing_Analysis_GUI_HighLift/src/GUI/Views/pr.java
@@ -0,0 +1,16 @@
+package GUI.Views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class pr extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
