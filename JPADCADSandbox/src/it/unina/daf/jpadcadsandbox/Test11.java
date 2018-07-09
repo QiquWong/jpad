406def17ef44c0de1b982f749adca5b71782b46f
Agostino De Marco <agostino.demarco@unina.it>
2018-07-09 16:17:32 +0200

    Photon

--- JPADCADSandbox/src/it/unina/daf/jpadcadsandbox/Test11.java
+++ JPADCADSandbox/src/it/unina/daf/jpadcadsandbox/Test11.java
@@ -19,62 +19,62 @@ import it.unina.daf.jpadcad.utils.FuselageSection;
 
 public class Test11 {
 
-	public static OCCShape makeFuselagePatch() {
-		
-		System.out.println("Section 1 ...");
-		FuselageSection<Amount<Length>> sec1 = FuselageSection.of(
-				Amount.valueOf( 1.0, SI.METER), // xStation
-				Amount.valueOf( 1.0, SI.METER), // semiWidth
-				Amount.valueOf( 0.0, SI.METER), // zBaseline
-				Amount.valueOf( 1.5, SI.METER), // aboveBaseline
-				Amount.valueOf( 0.8, SI.METER)  // belowBaseline				
-				);
-		System.out.println(sec1);
-
-		System.out.println("Section 2 ...");
-		FuselageSection<Amount<Length>> sec2 = FuselageSection.of(
-				Amount.valueOf( 6.0, SI.METER), // xStation
-				Amount.valueOf( 3.5, SI.METER), // semiWidth
-				Amount.valueOf( 0.0, SI.METER), // zBaseline
-				Amount.valueOf( 4.5, SI.METER), // aboveBaseline
-				Amount.valueOf( 2.8, SI.METER)  // belowBaseline				
-				);
-		System.out.println(sec2);
-		
-		List<CADGeomCurve3D> cadGeomCurveList = new ArrayList<CADGeomCurve3D>();
-		cadGeomCurveList.add(sec1.makeCADGeomCurve());
-		cadGeomCurveList.add(sec2.makeCADGeomCurve());
-		
-		// The CADShell object
-		System.out.println("Surfacing ...");
-		CADShell cadShell = OCCUtils.theFactory
-				                    .newShell(
-										cadGeomCurveList.stream() // purge the null objects
-									      				.filter(Objects::nonNull)
-									      				.collect(Collectors.toList())
-									);
-		return (OCCShape)cadShell;
-	}
-	
-	public static void main(String[] args) {
-		
-		MyConfiguration.customizeAmountOutput();
-		
-		System.out.println("Testing Java Wrapper of OCCT >= v7.0.0");
-		System.out.println("Classes in package it.unina.daf.jpadcad");
-		
-		System.out.println("========== Initialize CAD shape factory");
-		OCCUtils.initCADShapeFactory();
-		
-		System.out.println("========== Construct a fuselage nose");
-		OCCShape patch1 = Test11.makeFuselagePatch();
-
-		// Write to a file
-		String fileName = "test11.brep";
-
-		if (OCCUtils.write(fileName, ((OCCShape)patch1)))
-			System.out.println("Output written on file: " + fileName);
-		
-	}
+//	public static OCCShape makeFuselagePatch() {
+//		
+//		System.out.println("Section 1 ...");
+//		FuselageSection<Amount<Length>> sec1 = FuselageSection.of(
+//				Amount.valueOf( 1.0, SI.METER), // xStation
+//				Amount.valueOf( 1.0, SI.METER), // semiWidth
+//				Amount.valueOf( 0.0, SI.METER), // zBaseline
+//				Amount.valueOf( 1.5, SI.METER), // aboveBaseline
+//				Amount.valueOf( 0.8, SI.METER)  // belowBaseline				
+//				);
+//		System.out.println(sec1);
+//
+//		System.out.println("Section 2 ...");
+//		FuselageSection<Amount<Length>> sec2 = FuselageSection.of(
+//				Amount.valueOf( 6.0, SI.METER), // xStation
+//				Amount.valueOf( 3.5, SI.METER), // semiWidth
+//				Amount.valueOf( 0.0, SI.METER), // zBaseline
+//				Amount.valueOf( 4.5, SI.METER), // aboveBaseline
+//				Amount.valueOf( 2.8, SI.METER)  // belowBaseline				
+//				);
+//		System.out.println(sec2);
+//		
+//		List<CADGeomCurve3D> cadGeomCurveList = new ArrayList<CADGeomCurve3D>();
+//		cadGeomCurveList.add(sec1.makeCADGeomCurve());
+//		cadGeomCurveList.add(sec2.makeCADGeomCurve());
+//		
+//		// The CADShell object
+//		System.out.println("Surfacing ...");
+//		CADShell cadShell = OCCUtils.theFactory
+//				                    .newShell(
+//										cadGeomCurveList.stream() // purge the null objects
+//									      				.filter(Objects::nonNull)
+//									      				.collect(Collectors.toList())
+//									);
+//		return (OCCShape)cadShell;
+//	}
+//	
+//	public static void main(String[] args) {
+//		
+//		MyConfiguration.customizeAmountOutput();
+//		
+//		System.out.println("Testing Java Wrapper of OCCT >= v7.0.0");
+//		System.out.println("Classes in package it.unina.daf.jpadcad");
+//		
+//		System.out.println("========== Initialize CAD shape factory");
+//		OCCUtils.initCADShapeFactory();
+//		
+//		System.out.println("========== Construct a fuselage nose");
+//		OCCShape patch1 = Test11.makeFuselagePatch();
+//
+//		// Write to a file
+//		String fileName = "test11.brep";
+//
+//		if (OCCUtils.write(fileName, ((OCCShape)patch1)))
+//			System.out.println("Output written on file: " + fileName);
+//		
+//	}
 
 }
