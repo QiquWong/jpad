4a656a41f1ada970eef81a0b54a15bbc636ec416
Agostino De Marco <agostino.demarco@unina.it>
2019-02-05 18:57:16 +0100

    [JPADSandbox_ve] More examples for Atsi

--- JPADSandBox_v2/src/sandbox2/atsi/ATsiTestReadXml.java
+++ JPADSandBox_v2/src/sandbox2/atsi/ATsiTestReadXml.java
@@ -11,18 +11,26 @@ public class ATsiTestReadXml {
 
 	public static void main(String[] args) {
 
-		File file = new File("src/sandbox2/atsi/pippo.xml");
+		//File file = new File("src/sandbox2/atsi/pippo.xml");
+		File file = new File(args[0]);
+		
+		System.out.println("args[0]: " + args[0]);
+		System.out.println("File name: " + file.getAbsolutePath());
+		System.out.println("-------------");
 		
 		JPADXmlReader reader = new JPADXmlReader(file.getAbsolutePath());
 		
 		System.out.println(
-			"Name: " + reader.getXMLPropertyByPath("person/name") +
-			"Family name: " + reader.getXMLPropertyByPath("person/surname") +
-			"Height (m): " + reader.getXMLAmountAngleByPath("person/height").to(SI.METER)
+			"Name: " + reader.getXMLPropertyByPath("person/name") + "\n" +
+			"Family name: " + reader.getXMLPropertyByPath("person/surname") + "\n" +
+			"Height (m): " + reader.getXMLAmountAngleByPath("person/height").getEstimatedValue()
 		);
 		
 		String sex = MyXMLReaderUtils
-				.getXMLPropertyByPath(reader.getXmlDoc(), reader.getXpath(), "/person/sex/@value");
+				.getXMLPropertyByPath(
+						reader.getXmlDoc(), 
+						reader.getXpath(), 
+						"/person/sex/@value");
 		
 		String sex2 = reader.getXMLAttributeByPath("/person/sex","value");
 		
