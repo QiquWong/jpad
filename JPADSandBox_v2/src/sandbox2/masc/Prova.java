c4753cd3a6ececd2b4fbb8a67ca3996a7f67b5d5
Marcello Asciolla <marcelloasciolla@gmail.com>
2019-02-08 20:59:28 +0100

    Prima prova di commit

---
+++ JPADSandBox_v2/src/sandbox2/masc/Prova.java
@@ -0,0 +1,12 @@
+package sandbox2.masc;
+
+//Classe di prova per verifica funzionalità push/pull di git
+
+public class Prova {
+
+	public static void main(String[] args) {
+
+		System.out.println("Prova");
+	}
+
+}
