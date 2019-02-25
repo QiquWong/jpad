fc17706e348a1dd9f2f2fef05a7160f7f8dda5cc
Agostino De Marco <agostino.demarco@unina.it>
2019-02-25 18:48:40 +0100

    [JPADSandbox - Prince] Testing Matlab report generator

---
+++ JPADSandBox_v2/src/sandbox2/prince/Test02.java
@@ -0,0 +1,26 @@
+package sandbox2.prince;
+
+import ReportLib.*;
+import com.mathworks.toolbox.javabuilder.*;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Matlab report generator");
+		
+		ReportUtils theReportUtils = null;
+		Object[] result = null;
+		
+		try {
+			
+			theReportUtils = new ReportUtils();
+			
+			result = theReportUtils.generateReport(0, "Pippo1", "pdf", "template");
+			
+		} catch (MWException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
